e846e76c156d13d867bbb9c4776ccd5331af40a8
86181 <1312571788@qq.com>
2021-04-11 23:39:10 +0800

    week4+5+6

---
+++ src/main/java/com/chenyihao/week5/demo/LoginServlet.java
@@ -0,0 +1,67 @@
+package com.chenyihao.week5.demo;
+import java.sql.PreparedStatement;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    Connection con = null;
+    @Override
+    public void init() throws ServletException {
+
+//    public Connection dbConn;
+//    public void init() {
+//        try { Class.forName(getServletConfig().getServletContext().getInitParameter("driver"));
+//            dbConn = DriverManager.getConnection(getServletConfig().getServletContext().getInitParameter("url"), getServletConfig().getServletContext().getInitParameter("Username"), getServletConfig().getServletContext().getInitParameter("Password"));
+//        } catch (Exception e) {
+//            System.out.println(e); }
+        con =(Connection)getServletContext().getAttribute("dbConn");
+//        System.out.println(con);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+    @Override
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String password= request.getParameter("password");
+        System.out.println(name + password);
+//        System.out.println(con);
+        try {
+            if( con != null){
+                String sql = "SELECT * FROM usertable WHERE name=? AND password=?;";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1,name);
+                ps.setString(2,password);
+                ResultSet rs = ps.executeQuery();
+                if(rs.next()){
+                    // writer.println("Login Success!!!");
+                    // writer.println("Welcome "+name+".");
+                    request.setAttribute("ID",rs.getInt("id"));
+                    request.setAttribute("Username",rs.getString("name"));
+                    request.setAttribute("Password",rs.getString("password"));
+                    request.setAttribute("Email",rs.getString("email"));
+                    request.setAttribute("Gender",rs.getString("gender"));
+                    request.setAttribute("Birthdate",rs.getDate("birthdate"));
+                    request.getRequestDispatcher("userinfo.jsp").forward(request, response);
+                }else{
+                    request.setAttribute("msg" ,"username or password Error");
+                    request.getRequestDispatcher("login.jsp").forward(request,response);
+                    //  writer.print("Username or Password Error!!!");
+
+                }
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/main/java/com/chenyihao/week6/demo/EncondingFilter.java
@@ -0,0 +1,24 @@
+package com.chenyihao.week6.demo;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "Filter",value = "/*")
+public class EncondingFilter implements javax.servlet.Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+//        System.out.println("-------------------");
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+        chain.doFilter(req, resp);
+    }
+
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
